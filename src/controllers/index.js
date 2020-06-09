593e2888955c5e193792d7c60e5a1585e41f6d62
Vincent Iroleh <irolehiroleh@gmail.com>
2020-06-09 11:26:06 +0100

    chore(loginpage UI):add loginpagee UI [Finishes #6]

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,10 @@ class homeController {
   static home(req, res) {
     res.status(200).render('index');
   }
+
+  static login(req, res) {
+    res.status(200).render('login');
+  }
 }
 
 module.exports = homeController;
