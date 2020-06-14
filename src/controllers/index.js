f2d4235311a2a87f6f91d529b540855ff9cb6a11
Vincent Iroleh <irolehiroleh@gmail.com>
2020-06-14 16:24:53 +0100

    chore(dev dashboard):add admin dashboard UI [Finishes #8]]

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,6 +6,10 @@ class homeController {
   static login(req, res) {
     res.status(200).render('login');
   }
+
+  static dashboard(req, res) {
+    res.status(200).render('dashboard');
+  }
 }
 
 module.exports = homeController;
